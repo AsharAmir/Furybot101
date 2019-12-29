476501786049f4441e948d9a0d0fb54b624e939b
AsharAmir <59178154+AsharAmir@users.noreply.github.com>
2019-12-29 17:58:39 +0300

    Update botmain1.py

--- botmain1.py
+++ botmain1.py
@@ -173,7 +173,7 @@ async def m8b(ctx):
     
 @bot.command()
 async def rolladice(ctx):
-    await ctx.send(random.choice(["1", "2", "3", "4", "5", "6")] 
+    await ctx.send(random.choice(["1", "2", "3", "4", "5", "6"])) 
                               
 @bot.command()
 async def ok(ctx):
