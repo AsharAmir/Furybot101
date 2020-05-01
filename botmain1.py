049add3fa76eceffbf269b14f428621f18079a10
AsharAmir <59178154+AsharAmir@users.noreply.github.com>
2020-05-01 21:28:55 +0300

    Update botmain1.py

--- botmain1.py
+++ botmain1.py
@@ -53,7 +53,7 @@ async def plsinvite(ctx):
  
 @bot.command()
 async def plsserver(ctx):
-    count = len(bot.servers)
+    count = len(bot.guilds)
     await ctx.send('Im in + int(count)  + ' servers')
 
 # playing audio in a voice channel
